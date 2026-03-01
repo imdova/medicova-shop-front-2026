fc0bb1c8c0f22b4ef7079a75ffd1212bb04f590f
Mostafa <mostafa-for-work@hotmail.com>
2026-03-02 00:18:44 +0200

    server error fix

--- src/app/[locale]/(public)/(auth)/signin/page.tsx
+++ src/app/[locale]/(public)/(auth)/signin/page.tsx
@@ -11,7 +11,7 @@ import Link from "next/link";
 // UI text translations
 const translations = {
   title: {
-    en: "Login to your account",
+    en: "Login to your account 2",
     ar: "تسجيل الدخول إلى حسابك",
   },
   subtitle: {
